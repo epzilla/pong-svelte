152eeaab96bdbe56a6903b23d79dfe8ae115d602
Adam Epling <adamepling@gmail.com>
2021-08-12 00:06:25 -0500

    Fix casing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import FixedAlerts from '../components/FixedAlerts.svelte';
-  import Nav from '../components/nav.svelte';
+  import Nav from '../components/Nav.svelte';
 </script>
 
 <Nav />
